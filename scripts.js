f94e1b989800d9db8d7cb5abf7a6e358ca45b182
PushpaRai <Pushpathulungrai@gmail.com>
2022-04-13 21:19:23 +0545

    initial commit

---
+++ scripts.js
@@ -0,0 +1,23 @@
+
+alert("Hello TEJ!");
+
+function hello(){
+alert ("Hello there!");
+}
+
+
+function incrementValue()
+{
+var value = parseInt(document.getElementById('number').value);
+	value++;
+	document.getElementById('number').value = value;
+    
+}
+
+function decrementValue()
+{
+var value = parseInt(document.getElementById('number').value);
+ 	value--;
+	document.getElementById('number').value = value;
+
+}
